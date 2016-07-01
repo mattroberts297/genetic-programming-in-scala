9d7e62959df8609cab1ba9eee7e640f1ba14e617
Matt Roberts <mattr@cakesolutions.net>
2016-07-01 08:27:08 +0100

    Add ST type alias

--- src/main/scala/model/Exp.scala
+++ src/main/scala/model/Exp.scala
@@ -4,7 +4,7 @@ import scala.collection.immutable._
 import scala.util.Try
 
 sealed trait Exp {
-  def eval(implicit symbols: Map[Symbol, Float]): Float
+  def eval(implicit symbols: ST): Float
 }
 
 trait BinOp {
@@ -13,37 +13,37 @@ trait BinOp {
 }
 
 case class Add(lhs: Exp, rhs: Exp) extends Exp with BinOp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = {
+  override def eval(implicit symbols: ST): Float = {
     lhs.eval + rhs.eval
   }
 }
 
 case class Sub(lhs: Exp, rhs: Exp) extends Exp with BinOp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = {
+  override def eval(implicit symbols: ST): Float = {
     lhs.eval - rhs.eval
   }
 }
 
 case class Mul(lhs: Exp, rhs: Exp) extends Exp with BinOp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = {
+  override def eval(implicit symbols: ST): Float = {
     lhs.eval * rhs.eval
   }
 }
 
 case class Div(lhs: Exp, rhs: Exp) extends Exp with BinOp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = Try {
+  override def eval(implicit symbols: ST): Float = Try {
     lhs.eval / rhs.eval
   } getOrElse 1f
 }
 
 case class Con(value: Float) extends Exp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = {
+  override def eval(implicit symbols: ST): Float = {
     value
   }
 }
 
 case class Var(name: Symbol) extends Exp {
-  override def eval(implicit symbols: Map[Symbol, Float]): Float = {
+  override def eval(implicit symbols: ST): Float = {
     symbols(name)
   }
 }
---
+++ src/main/scala/model/package.scala
@@ -0,0 +1,7 @@
+
+import scala.collection.immutable._
+import scala.collection.mutable
+
+package object model {
+  type ST = Map[Symbol, Float]
+}
